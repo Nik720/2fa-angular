30b82915b5dbee777ab0209c4e8c550cacb03087
Nikunj Beladiya <ndbeladiya720@gmail.com>
2024-04-16 11:35:31 +0530

    Added registration page design

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
-
 }
