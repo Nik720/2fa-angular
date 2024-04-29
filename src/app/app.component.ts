787e48ae160db34aa55ac7941bfa8ccc7e8c0ccc
Nikunj Beladiya <ndbeladiya720@gmail.com>
2024-04-29 10:59:11 +0530

    handled header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AlertsComponent } from './components/alerts/alerts.component';
 import { CommonModule } from '@angular/common';
 import { AuthenticationService } from './services/authentication.service';
 import { User } from './models/user';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -19,18 +20,29 @@ import { User } from './models/user';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'two factor authentication';
   user? : User | null;
   isLogin? = false;
+  currentUrl? = '';
 
   constructor(
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private router: Router
   ) {
-    this.authService.user.subscribe((u) => {
-      this.user = u;
-      this.isLogin = u?.id ? !this.isLogin : false;
-    });
+    
+  }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event:any) => {
+        this.currentUrl = event.url;
+        this.authService.user.subscribe((u) => {
+          this.user = u;
+          this.isLogin = u?.id && !this.currentUrl?.includes('/validateOtp') ? !this.isLogin : false;
+        });
+      });
   }
 
   logout() {
